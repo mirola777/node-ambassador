a7076793b9ce1768cc71b866d41bdc0bf558b6a8
mirola777 <jmmunoza@eafit.edu.co>
2024-10-20 10:15:59 -0500

    Kafka added to services

--- email-adapter/src/index.ts
+++ email-adapter/src/index.ts
@@ -1,7 +1,50 @@
 import dotenv from "dotenv";
+import { EachMessagePayload, Kafka } from "kafkajs";
+import { createTransport } from "nodemailer";
 
 dotenv.config();
 
-const port = process.env.PORT || 8000;
+const kafka = new Kafka({
+  clientId: "email-client",
+  brokers: ["host.docker.internal:9094"],
+  sasl: {
+    mechanism: "plain",
+    username: "user",
+    password: "bitnami",
+  },
+});
 
-console.log("Email Adapter is starting...");
+const consumer = kafka.consumer({ groupId: "email-consumer" });
+
+const transporter = createTransport({
+  host: "host.docker.internal",
+  port: 1025,
+});
+
+const run = async () => {
+  await consumer.connect();
+  await consumer.subscribe({ topic: "test" });
+  await consumer.run({
+    eachMessage: async (message: EachMessagePayload) => {
+      const order = JSON.parse(message.message.value.toString());
+
+      await transporter.sendMail({
+        from: "from@example.com",
+        to: "admin@admin.com",
+        subject: "An order has been completed",
+        html: `Order #${order.id} with a total of $${order.admin_revenue} has been completed`,
+      });
+
+      await transporter.sendMail({
+        from: "from@example.com",
+        to: order.ambassador_email,
+        subject: "An order has been completed",
+        html: `You earned $${order.ambassador_revenue} from the link #${order.code}`,
+      });
+    },
+  });
+
+  await transporter.close();
+};
+
+run().then(console.error);
--- firebase-auth-adapter/src/index.ts
+++ firebase-auth-adapter/src/index.ts
@@ -1,7 +1,30 @@
 import dotenv from "dotenv";
+import { EachMessagePayload, Kafka } from "kafkajs";
 
 dotenv.config();
 
 const port = process.env.PORT || 8000;
 
-console.log("Firebase Auth Adapter is starting...");
+const kafka = new Kafka({
+  clientId: "email-client",
+  brokers: ["host.docker.internal:9094"],
+  sasl: {
+    mechanism: "plain",
+    username: "user",
+    password: "bitnami",
+  },
+});
+
+const consumer = kafka.consumer({ groupId: "email-consumer" });
+
+const run = async () => {
+  await consumer.connect();
+  await consumer.subscribe({ topic: "test" });
+  await consumer.run({
+    eachMessage: async (message: EachMessagePayload) => {
+      const order = JSON.parse(message.message.value.toString());
+    },
+  });
+};
+
+run().then(console.error);
