d2fbd61abab148690f183bf468d273bc5f7643b6
mirola777 <jmmunoza@eafit.edu.co>
2024-10-20 11:01:26 -0500

    Adding data to checkout

---
+++ checkout-microservice/src/routes.ts
@@ -0,0 +1,24 @@
+import { Router } from "express";
+import {
+  CreateLink,
+  GetLink,
+  Links,
+  Stats,
+} from "./controller/link.controller";
+import {
+  ConfirmOrder,
+  CreateOrder,
+  Orders,
+} from "./controller/order.controller";
+import { Rankings } from "./controller/user.controller";
+
+export const routes = (router: Router) => {
+  router.get("/api/stats", Stats);
+  router.get("/api/rankings", Rankings);
+  router.get("/api/users/:id/links", Links);
+  router.post("/api/links", CreateLink);
+  router.get("/api/links/:code", GetLink);
+  router.post("/api/orders", CreateOrder);
+  router.get("/api/orders", Orders);
+  router.post("/api/orders/confirm", ConfirmOrder);
+};
