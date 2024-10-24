2a317eddab47d66e480d475e0ce232b9d350f10e
mirola777 <jmmunoza@eafit.edu.co>
2024-10-24 16:26:54 -0500

    Refactor package dependencies for API Gateway

--- api-gateway/src/index.ts
+++ api-gateway/src/index.ts
@@ -1,4 +1,6 @@
 import console from "console";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 import { AuthMiddleware } from "./auth.middleware";
@@ -9,7 +11,18 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cookieParser());
 app.use(express.json());
+app.use(
+  cors({
+    credentials: true,
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:4000",
+      "http://localhost:5000",
+    ],
+  })
+);
 app.all(
   "/api/:client/:microservice/*",
   AuthMiddleware,
